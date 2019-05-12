ba5ba96e832865eec7cf2c2f8977534811b53e78
Surya Das <surya.das@outlook.com>
2019-05-12 21:56:38 +1000

    add app insights

--- worthWatchingAPI/Program.cs
+++ worthWatchingAPI/Program.cs
@@ -19,6 +19,7 @@ namespace worthWatchingAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
